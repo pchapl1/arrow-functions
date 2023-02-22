eec9eb52cf93334a851cb9401633c950ab5d838d
Phil Chaplin <pchapl1@gmail.com>
2023-02-22 09:36:26 -0800

    finsihed the refactoring assignment

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ Student.courseName = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -180,17 +180,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// an instance of a student object
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// an instance of a Window object
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// because arrow functions are not bound to "this" like the other functions.  it will throw a typeError
